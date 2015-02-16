f744fdc999b442fda9ed06d7bb600ace4705ce84
dplin <derek.pc.lin@gmail.com>
2015-02-16 02:14:43 -0500

    Updated base.txt and settings

--- woola/config/common.py
+++ woola/config/common.py
@@ -111,7 +111,19 @@ class Common(Configuration):
 
     # DATABASE CONFIGURATION
     # See: https://docs.djangoproject.com/en/dev/ref/settings/#databases
-    DATABASES = values.DatabaseURLValue('postgres://localhost/woola')
+    #DATABASES = values.DatabaseURLValue('postgres://postgres:root@localhost/red')
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+            'NAME': 'red',                      # Or path to database file if using sqlite3.
+            'USER': 'postgres',                      # Not used with sqlite3.
+            'PASSWORD': 'root',                  # Not used with sqlite3.
+            'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
+            'PORT': '5432',                      # Set to empty string for default. Not used with sqlite3.
+        }
+    } 
+    
+    
     # END DATABASE CONFIGURATION
 
     # CACHING
